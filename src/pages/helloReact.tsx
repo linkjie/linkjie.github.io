eabe589688228fdf94ed7317d37e83ab6fc67018
linkangjie <linkangjie9726@163.com>
2024-10-30 15:49:10 +0800

    first

---
+++ src/pages/helloReact.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Hello() {
+  return (
+    <Layout title="Hello" description="Hello React Page" key={111}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <p>
+          hellohellohellohellohellohellohello
+        </p>
+      </div>
+    </Layout>
+  );
+}
