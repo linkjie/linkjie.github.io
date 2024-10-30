9760cd7538490ba8d51fb03817203a19beb3cd12
linkangjie <linkangjie9726@163.com>
2024-10-30 16:40:37 +0800

    commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,8 +15,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'linkjie', // Usually your GitHub org/user name.
+  projectName: 'linkjie.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
