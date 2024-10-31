b74a349402936c0e27cfa72ab9fc831df21cb9bf
linkangjie <linkangjie9726@163.com>
2024-10-31 11:33:55 +0800

    onBrokenLinks

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   deploymentBranch: "master",
   trailingSlash: false,
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
