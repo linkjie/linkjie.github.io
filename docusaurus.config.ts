22275ad2aef82b1b270d89ce019f8848ea7061f9
linkangjie <linkangjie9726@163.com>
2025-04-25 14:25:39 +0800

    commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -179,26 +179,17 @@ const config: Config = {
 
       indexName: "test2_pages",
 
-      // 可选：见下文
-      contextualSearch: false,
+      // 可选
+      contextualSearch: true, //上下文搜索
 
-      // 可选：声明哪些域名需要用 window.location 型的导航而不是 history.push。 适用于 Algolia 配置会爬取多个文档站点，而我们想要用 window.location.href 在它们之间跳转时。
-      // externalUrlRegex: 'external\\.com|domain\\.com',
+      // 可选：Algolia 搜索时自定义的额外参数
+      // searchParameters: {},
 
-      // 可选：替换 Algolia 的部分网址。 在使用相同搜索索引支持多个不同 baseUrl 的部署时非常有用。 你可以在 “from” 中使用正则表达式或字符串。 比方说，localhost:3000 和 myCompany.com/docs
-      // replaceSearchResultPathname: {
-      //   from: '/docs/', // or as RegExp: /\/docs\//
-      //   to: '/',
-      // },
+      // 可选：默认启用的搜索页路径（传递 `false` 以禁用它）启用时会新增一个/search搜索页
+      // searchPagePath: "search",
 
-      // 可选：Algolia 搜索参数
-      searchParameters: {},
-
-      // 可选：默认启用的搜索页路径（传递 `false` 以禁用它）
-      searchPagePath: "search",
-
-      // 可选：Docsearch 的 insights 功能是否启用（默认为 `false`）
-      insights: false,
+      // // 可选：Docsearch 的 insights 功能是否启用（默认为 `false`）
+      // insights: false,
 
       //... 其他 Algolia 参数
     },
